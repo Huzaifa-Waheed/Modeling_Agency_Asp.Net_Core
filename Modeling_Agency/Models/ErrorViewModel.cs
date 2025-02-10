47aaaea50b26be00528f3531c5cbff6109d07874
Muhammad Huzaifa <huzaifawaheed2727@gmail.com>
2025-02-10 21:35:52 +0500

    Add project files.

---
+++ Modeling_Agency/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modeling_Agency.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
