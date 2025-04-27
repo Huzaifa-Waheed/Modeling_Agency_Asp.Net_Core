fbe11667e912ecc44c93e59af9ee055b57d1342f
Muhammad Huzaifa <huzaifawaheed2727@gmail.com>
2025-04-27 22:50:08 +0500

    Make some pages and apply CSS and JS on them.

--- Modeling_Agency/wwwroot/js/site.js
+++ Modeling_Agency/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// Show notifications modal
+$(document).ready(function () {
+    $('#notifications-button').click(function () {
+        $('#notificationsModal').modal('show');
+    });
+})
