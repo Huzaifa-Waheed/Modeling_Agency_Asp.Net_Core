320846fb69f1a91718d7cf86954e350ae8841d65
Muhammad Huzaifa <huzaifawaheed2727@gmail.com>
2025-04-27 22:49:17 +0500

    Admin Views and their implementations

---
+++ Modeling_Agency/Models/ViewModels/ModelVM.cs
@@ -0,0 +1,12 @@
+﻿using Modeling_Agency.Models.DbModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Modeling_Agency.Models.ViewModels
+{
+    public class ModelVM
+    {
+        public Model model { set; get; }
+        public IEnumerable<HireRecord> hireRecords { set; get; }
+        public IEnumerable<Messages> messages { set; get; }
+    }
+}
